e254e4774256a9367257f75d3882c0bc7dd93050
bw4sz <benweinstein2010@gmail.com>
2018-12-11 14:57:02 -0800

    poisson analysis

--- model/poisson_interactions.R
+++ model/poisson_interactions.R
@@ -1,37 +1,12 @@
-sink("model/poisson_interactions_detection.jags")
+sink("model/poisson_interactions.jags")
 cat("
     model {
-    
-    #Latent Interaction probability
-    for (i in 1:Birds){
-    for (j in 1:Plants){
-    #P(Interaction)
-    logit(phi[i,j])<-alpha[Bird[i],Plant[j]] 
-    } 
-    }
-    
-    
-    # Bird presence at each camera (placed on a single flower Plant (j))
-    for (i in 1:Birds){
-    for( j in 1:Plants){
-    for(k in 1:Cameras){
-    
-    #Latent Interaction State for each camera, conditional on presence
-    eff.phi[i,j,k] = occ[i,j,k] * phi[i,j]
-    z[i,j,k]~dbern(eff.phi[i,j,k])
-    } 
-    }
-    }
     
     #Observed interactions
     
     for (x in 1:Nobs){
-    
-    #Probability of detection, conditional on presence and interaction
-    psi[x] = z[Bird[x],Plant[x],Camera[x]] * omega[Bird[x]]
-    
     #Interaction Observation
-    Yobs[x] ~ dbern(psi[x])
+    Yobs[x] ~ dpois(lambda[Bird[x],Plant[x]])
     }
     
     #Assess Model Fit
@@ -39,20 +14,11 @@ cat("
     
     #Species level priors
     for (i in 1:Birds){
-    
-    #Detection prior
-    omega[i] ~ dbeta(1,1)
-    
-    #Flat elevation intercept prior
-    alpha_elev[i] ~ dnorm(0,0.386)
-    
-    #Elevation presence priors
-    beta[i] ~ dnorm(0,0.386)
-    
+    #Intensity prior
     for (j in 1:Plants){
     
     #Flat interaction prior
-    alpha[i,j] ~ dbeta(1,1)
+    lambda[i,j] ~ dunif(0,20)
     } 
     }
     
--- model/poisson_interactions_detection.R
+++ model/poisson_interactions_detection.R
@@ -2,14 +2,6 @@ sink("model/poisson_interactions_detection.jags")
 cat("
     model {
     
-    #Latent Interaction probability
-    for (i in 1:Birds){
-    for (j in 1:Plants){
-    #Intensity of Interaction
-    lambda[i,j]<-alpha[Bird[i],Plant[j]] 
-    } 
-    }
-    
     # Bird presence at each camera (placed on a single flower Plant (j))
     for (i in 1:Birds){
     for( j in 1:Plants){
@@ -39,12 +31,13 @@ cat("
     #Detect priors, logit transformed - Following lunn 2012 p85
     logit(omega[i])<-dcam[i]
     dcam[i]~dnorm(omega_mu,omega_tau)
+    #omega[i] ~ dbeta(1,1)
 
     #Intensity prior
     for (j in 1:Plants){
     
     #Flat interaction prior
-    alpha[i,j] ~ dunif(0,20)
+    lambda[i,j] ~ dunif(0,20)
     } 
     }
     
