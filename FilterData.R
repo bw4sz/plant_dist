9019b7d654a2e5a2d8443750d0e2008e37b97ebc
bw4sz <benweinstein2010@gmail.com>
2018-12-12 10:37:55 -0800

    new filtering dates

--- FilterData.R
+++ FilterData.R
@@ -1,5 +1,4 @@
 #Filter data
-
 library(anytime)
 library(chron)
 library(ggplot2)
@@ -8,7 +7,6 @@ library(tidyr)
 library(taxize)
 library(lubridate)
 library(dplyr)
-
 
 #Plant list
 gesner<-c("Columnea ciliata","Columnea kucyniakii","Drymonia tenuis","Columnea mastersonii","Gasteranthus quitensis","Drymonia teuscheri",
@@ -31,10 +29,36 @@ transects_2013$Date<-as.POSIXct(transects_2013$Date)
 #2017
 transects_2017<-read.csv("data/PlantTransects.csv",row.names=1)
 transects_2017<-transects_2017  %>% filter(final_plant_name %in% gesner,site %in% c("Maquipucuna","SantaLuciaUpper","SantaLuciaLower") ) %>% 
-select(Plant=final_plant_name,Flowers=total_flowers,Date=date,lon,lat,ele) 
+select(Plant=final_plant_name,Flowers=total_flowers,Date=date,lon,lat,ele)  %>% mutate(Date=as.character(Date))
+
+#Do some date cleaning
+transects_2017$Date[str_detect(transects_2017$Date,"/18")]<-as.character(as.POSIXct(transects_2017$Date[str_detect(transects_2017$Date,"/18")],format="%m/%d/%y"))
+transects_2017$Date[str_detect(transects_2017$Date,"/17")]<-as.character(as.POSIXct(transects_2017$Date[str_detect(transects_2017$Date,"/17")],format="%m/%d/%y"))
+
+#one crazy date
+transects_2017$Date[transects_2017$Date %in% "24//10/2018"]<-"24/10/2018"
+
+#if no slashes, its a number
+check_date<-function(x){
+  print(x)
+  if(str_detect(x,"\\/")){
+    newdate<-as.POSIXct(x,format="%d/%m/%Y")
+    return(newdate)
+  } 
+  else{
+    newdate<-anydate(x)
+    return(newdate)
+  }
+}
+
+for(x in 1:nrow(transects_2017)){
+  transects_2017$Date[x]<-as.character(check_date(transects_2017$Date[x]))
+}
+
+#Enfore posix
+transects_2017$Date<-as.POSIXct(transects_2017$Date)
 
 #date columns
-transects_2017$Date<-as.POSIXct(transects_2017$Date,format="%d/%m/%Y")
 transects<-bind_rows(transects_2013,transects_2017)
 
 #reduce observer effects, take out karen's data
@@ -43,42 +67,5 @@ transects<-transects %>% filter(!Observer %in% "Karen") %>% select(-Observer)
 #view dates
 transects %>% group_by(Date,Plant) %>% dplyr::summarize(s=sum(Flowers)) %>% ggplot(.,aes(x=Date,y=s)) + facet_wrap(~Plant,scales="free") + geom_point() +theme_bw()
 
-#seaonally
-transects %>% mutate(Month=months(Date),Year=years(Date)) %>% group_by(Year,Month,Plant) %>% dplyr::summarize(s=sum(Flowers)) %>% ggplot(.,aes(x=Month,y=s,col=Year)) + facet_wrap(~Plant,scales="free") + geom_line(aes(group=Year)) +theme_bw() 
-
-#julian day
-transects<-transects %>% mutate(Month=months(Date),Year=chron::years(Date),julian=yday(Date)) %>% filter(Year > 2013)
-
-transects[transects$Year %in% "17","Year"]<-2017
-transects[transects$Year %in% "18","Year"]<-2018
-
-
-ggplot(transects,aes(x=julian,y=Flowers)) + facet_wrap(~Plant,scales="free") + geom_line()
-
-#write interaction files
+#write files
 write.csv(transects,"data/cleaned/transects.csv")
-
-##Elevation ranges, use all plant species
-interactions<-interactions %>% select(ID,Date,Plant,ele,Hummingbird)
-
-#Impute non-detections. Start by assuming all hummingbirds occur at all elevations (can be relaxed)
-dailydat<-interactions %>% group_by(ID,Date,ele,Plant,Hummingbird) %>% summarize(n=n()) %>% mutate(Yobs=(n>0)*1) %>% select(-n) %>% filter(Hummingbird %in% ginteractions$Hummingbird )
-
-dailydat$Hummingbird<-as.factor(dailydat$Hummingbird)
-sumf<-dailydat %>% group_by(Plant,ID,Date,ele) %>% dplyr::summarize(n=n())
-
-#Create emptys zeros
-impute_zero<-function(x,Hummingbird){
-  df<-data.frame(Hummingbird=Hummingbird)
-}
-
-zero_count<-sumf %>% group_by(Plant,ID,Date,ele) %>% do(impute_zero(.,Hummingbird=levels(dailydat$Hummingbird)))
-zero_count<-zero_count %>% anti_join(dailydat)
-
-#add a dummy label for presence absence
-elev_zero<-bind_rows(data.frame(dailydat),data.frame(zero_count,Yobs=0)) %>% filter(!Plant %in% gesner,!is.na(ele))
-ggplot(elev_zero,aes(x=ele,y=Yobs)) + geom_point() + facet_wrap(~Hummingbird)
-
-#drop gesner species, not to duplicate data
-
-write.csv(elev_zero,"data/elevationdata.csv")
---
+++ functions.R
@@ -0,0 +1,52 @@
+#Source functions
+tauC_repulsion<-function(omega,gamma,I,D,lambda_cov){
+  C = exp(-lambda_cov * D)
+  vCov=omega*C + (1-omega) * I
+  result<-solve(vCov*gamma)
+  result<-cov2cor(result)
+  result<-reshape2::melt(result)
+  return(result)
+}
+
+tauC_attraction<-function(omega,gamma,I,D,lambda_cov){
+  C = exp(-lambda_cov * D)
+  vCov=omega*C + (1-omega) * I
+  result<-vCov*gamma
+  result<-cov2cor(result)
+  result<-reshape2::melt(result)
+  return(result)
+}
+effect_repulsion<-function(lambda,D,omega,gamma){
+  C<-exp(-lambda*D)
+  vCov=(omega*C[,] + (1-omega) * I)
+  vCov=solve(vCov*gamma)
+  data.frame(lambda,Distance=as.numeric(D),Covariance=as.numeric(vCov))
+}
+
+effect<-function(lambda,D,omega,gamma){
+  C<-exp(-lambda*D)
+  vCov=(omega*C[,] + (1-omega) * I)
+  vCov=vCov*gamma
+  data.frame(lambda,Distance=as.numeric(D),Covariance=as.numeric(vCov))
+}
+
+cor_effect<-function(lambda,D,omega,gamma){
+  C<-exp(-lambda*D)
+  vCov=(omega*C[,] + (1-omega) * I)
+  vCov=vCov*gamma
+  cormatrix<-cov2cor(vCov)
+  data.frame(lambda,Distance=as.numeric(D),Correlation=as.numeric(cormatrix))
+}
+
+cor_effect_repulsion<-function(lambda,D,omega,gamma){
+  C<-exp(-lambda*D)
+  vCov=(omega*C[,] + (1-omega) * I)
+  vCov=solve(vCov*gamma)
+  cormatrix<-cov2cor(vCov)
+  data.frame(lambda,Distance=as.numeric(D),Correlation=as.numeric(cormatrix))
+}
+
+all_plants<-function(dat){
+  data.frame(dat,Plant=unique(sumf$Plant),n=0)
+}
+
